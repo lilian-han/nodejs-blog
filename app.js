cbc88d6c6ab8acbe0390f5be9d38bbe88e07a274
han <15162294433@163.com>
2020-07-13 10:07:05 +0800

    test

--- app.js
+++ app.js
@@ -1,6 +1,100 @@
+const queryString = require('querystring')
+
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
+// 用于处理 post data
+const getPostData = (req) => {
+  const promise = new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      resolve({})
+      return
+    }
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({})
+      return
+    }
+
+    let postData = ''
+    req.on('data', (chunk) => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return
+      }
+      resolve(JSON.parse(postData))
+    })
+  })
+  return promise
+}
+
 const serverHandle = (req, res) => {
   // 设置返回格式
   res.setHeader('Content-type', 'application/json')
+
+  // 获取 path
+  const url = req.url
+  req.path = url.split('?')[0]
+
+  // 解析 query
+  req.query = queryString.parse(url.split('?')[1])
+
+  // 解析 cookie
+  req.cookie = {}
+  const cookieStr = req.headers.cookie || ''
+  cookieStr.split(';').forEach((item) => {
+    if (!item) {
+      return
+    }
+    const arr = item.split('=')
+    const key = arr[0].trim()
+    const val = arr[1].trim()
+    req.cookie[key] = val
+  })
+  console.log('serverHandle -> req.cookie', req.cookie)
+
+  // 处理 post data
+  getPostData(req).then((postData) => {
+    req.body = postData
+
+    // 处理 blog 路由
+    // const blogData = handleBlogRouter(req, res)
+    // if (blogData) {
+    //   res.end(JSON.stringify(blogData))
+    //   return
+    // }
+    const bolgResult = handleBlogRouter(req, res)
+    if (bolgResult) {
+      bolgResult.then((bolgData) => {
+        res.end(JSON.stringify(bolgData))
+      })
+      return
+    }
+
+    // 处理 user 路由
+    // const userData = handleUserRouter(req, res)
+    // if (userData) {
+    //   res.end(JSON.stringify(userData))
+    //   return
+    // }
+
+    const userResult = handleUserRouter(req, res)
+    if (userResult) {
+      userResult.then((userData) => {
+        res.end(JSON.stringify(userData))
+      })
+      return
+    }
+
+    // 未命中路由，返回 404
+    res.writeHead(404, {
+      'Content-type': 'text/plain',
+    })
+    res.write('404 Not Found\n')
+    res.end()
+  })
 }
 
 module.exports = serverHandle
---
+++ src/router/user.js
@@ -0,0 +1,46 @@
+const login = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000)
+  return d.toGMTString()
+}
+
+const handleUserRouter = (req, res) => {
+  const method = req.method
+
+  // 登录
+  if (method === 'GET' && req.path === '/api/user/login') {
+    // const { username, password } = req.body
+    const { username, password } = req.query
+    const result = login(username, password)
+    return result.then((data) => {
+      if (data.username) {
+        // 操作 cookie
+        res.setHeader(
+          'Set-Cookie',
+          `username=${
+            data.username
+          }; path=/; httpOnly; expires=${getCookieExpires()}`
+        )
+        return new SuccessModel('登录成功！')
+      } else {
+        return new ErrorModel('登陆失败！')
+      }
+    })
+  }
+
+  // 登录验证的测试
+  if (method === 'GET' && req.path === '/api/user/login-test') {
+    if (req.cookie.username) {
+      return Promise.resolve(
+        new SuccessModel({ username: req.cookie.username })
+      )
+    }
+    return Promise.resolve(new ErrorModel('尚未登录'))
+  }
+}
+
+module.exports = handleUserRouter
